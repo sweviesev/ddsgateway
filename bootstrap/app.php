16ba2554cf94fe6aed2b0e2b3607bf4f46236c97
sweviesev <raut.kirstian@gmail.com>
2025-04-12 14:51:36 +0800

    deploy changes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,38 +8,16 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| Here we will load the environment and create the application instance
-| that serves as the central piece of this framework. We'll use this
-| application as an "IoC" container and router for this framework.
-|
-*/
-
 $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
 $app->withFacades();
-
 $app->withEloquent();
 
 $app->configure('services');
-
-
-/*
-|--------------------------------------------------------------------------
-| Register Container Bindings
-|--------------------------------------------------------------------------
-|
-| Now we will register a few bindings in the service container. We will
-| register the exception handler and the console kernel. You may add
-| your own bindings here if you like or you can make another file.
-|
-*/
+$app->configure('auth');
+$app->configure('app');
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
@@ -51,70 +29,20 @@ $app->singleton(
     App\Console\Kernel::class
 );
 
-/*
-|--------------------------------------------------------------------------
-| Register Config Files
-|--------------------------------------------------------------------------
-|
-| Now we will register the "app" configuration file. If the file exists in
-| your configuration directory it will be loaded; otherwise, we'll load
-| the default version. You may register other files below as needed.
-|
-*/
-
-$app->configure('app');
-$app->configure('auth');
-
-
-
-/*
-|--------------------------------------------------------------------------
-| Register Middleware
-|--------------------------------------------------------------------------
-|
-| Next, we will register the middleware with the application. These can
-| be global middleware that run before and after each request into a
-| route or middleware that'll be assigned to some specific routes.
-|
-*/
-
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
-
+// Register route middleware, including the new gateway authentication middleware.
 $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
-    'client.credentials' => Laravel\Passport\Http\Middleware\CheckClientCredentials::class,
+    'gateway.auth' => App\Http\Middleware\GatewayAuthenticate::class,
 ]);
 
-/*
-|--------------------------------------------------------------------------
-| Register Service Providers
-|--------------------------------------------------------------------------
-|
-| Here we will register all of the application's service providers which
-| are used to bind services into the container. Service providers are
-| totally optional, so you are not required to uncomment this line.
-|
-*/
-
-//$app->register(App\Providers\AppServiceProvider::class);
+// Register Service Providers
 $app->register(App\Providers\AuthServiceProvider::class);
-$app->register(Laravel\Passport\PassportServiceProvider::class);
-//$app->register(App\Providers\EventServiceProvider::class);
-$app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
 
-/*
-|--------------------------------------------------------------------------
-| Load The Application Routes
-|--------------------------------------------------------------------------
-|
-| Next we will include the routes file so that they can all be added to
-| the application. This will provide all of the URLs the application
-| can respond to, as well as the controllers that may handle them.
-|
-*/
+// Remove any Passport providers:
+// $app->register(Laravel\Passport\PassportServiceProvider::class);
+// $app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
 
+// Register your routes:
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
